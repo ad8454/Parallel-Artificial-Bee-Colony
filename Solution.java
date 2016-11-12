ea2067cc2d8305d53e8937d911c2a634039967e5
= <sameer.raghuram@gmail.com>
2016-11-12 14:17:09 -0500

    add exploitation function with proper swaps

--- Solution.java
+++ Solution.java
@@ -12,6 +12,7 @@ import java.util.Random;
 class Solution implements Cloneable, Comparable<Solution>{
 
     // shared variables
+	private Node[] allNodes;
 	private Node route[];	// Solution path eg {0->1->2->3->0->4->5->6->0->7->8}
 	private int totalNodes;	// Total nodes
 	private double fitness = -1;
@@ -31,6 +32,7 @@ class Solution implements Cloneable, Comparable<Solution>{
 	 */
 	public Solution(Node allNodes[], int totVehicles, int id){
         this.id = id;
+		this.allNodes = allNodes;
 		totalNodes = allNodes.length;
 		route = new Node[totalNodes + totVehicles];
 		for(int i=0; i<route.length; i++){
@@ -73,9 +75,8 @@ class Solution implements Cloneable, Comparable<Solution>{
 		return this.route;
 	}
 	public void setRoute(Node[] route){
-		for(int i = 0; i<route.length; i++){
-			this.route[i] = route[i];
-		}
+
+		this.route = route;
 	}
 
     public void setTrial(int trial){
@@ -118,6 +119,27 @@ class Solution implements Cloneable, Comparable<Solution>{
 			swap(i, idx2);
 		}
 		//this.fitness = computeFitness();
+	}
+
+	public double exploitSolution(Random rand){
+		double oldFitness = computeFitness();
+		int idx1 = rand.nextInt(route.length - 2) + 1;
+		int idx2 = rand.nextInt(route.length - 2) + 1;
+		swap(idx1, idx2);
+		double newFitness = computeFitness();
+		if(oldFitness > newFitness) {
+			// Increment the number of trials to indicate exhaustion of
+			// a food source
+			incTrial();
+			swap(idx1, idx2); //revert
+			newFitness = oldFitness;		//revert to old fitness
+		}
+		// reset the number of trials of solution to indicate improvement
+		if(!(oldFitness == newFitness)){
+			setTrial(0);
+		}
+		setFitness(newFitness);
+		return this.fitness;
 	}
 
 	/**

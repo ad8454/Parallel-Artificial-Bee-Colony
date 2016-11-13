c45214ae4a8cb35afbeb31c167e428e2ab3793e1
= <sameer.raghuram@gmail.com>
2016-11-12 19:08:29 -0500

    added parallel for construct

--- ABCSmp.java
+++ ABCSmp.java
@@ -1,13 +1,82 @@
+import edu.rit.pj2.Loop;
 import edu.rit.pj2.Task;
+import edu.rit.util.Instance;
+
+import java.util.Random;
 
 /**
  * Created by Sameer on 11/12/2016.
  */
 public class ABCSmp extends Task{
 
+    int totEmployedBees = 100;		//TODO: find way to determine
+    int MAX_EPOCH = 10000;
+    int totVehicles = 4;
+    Graph graph = null;
+    Random rand;
+    int lb=0;
+    int ub = totEmployedBees;
+    SolutionVbl employedBees[] = new SolutionVbl[totEmployedBees];
+    SolutionVbl onlookerBees[] = new SolutionVbl[totEmployedBees];
+    SolutionVbl bestDiscarded = new SolutionVbl();
 
     public void main(String args[]){
 
+        if (args.length != 1) {
+            usage(0);
+        }
 
+        // Get Graph instance
+        try{
+            graph = (Graph) Instance.newInstance (args[0]);
+        } catch(Exception e){
+            usage(1);
+        }
+
+        rand = new Random();
+
+        // Get total vertices
+        int totNodes = graph.getNodes();
+        Node allNodes[] = new Node[totNodes];
+
+        // Get all nodes in graph
+        for(int i=0; i<totNodes; i++){
+            allNodes[i] = new Node();
+            graph.nextVertex(allNodes[i]);
+        }
+
+        // Generate initial solutions
+        for(int i=0; i<totEmployedBees; i++){
+            //employedBees[i] = new Bee(totNodes, totVehicles);
+            employedBees[i].item = new Solution(allNodes, totVehicles, i);
+            employedBees[i].item.genRandomSolution(rand);
+            //System.out.println(employedBees[i]+"\n");
+            onlookerBees[i].item = new Solution();
+        }
+
+        int epoch = 0;
+
+        while(epoch++ < MAX_EPOCH){
+            // Initiate a parallelFor loop across all cores.
+            // Each core will handle a different solution
+            parallelFor(lb, ub).exec(new Loop() {
+
+                SolutionVbl thrBestDiscarded;
+                Random thrRand;
+
+                public void start(){
+                    thrBestDiscarded = threadLocal(bestDiscarded);
+                    thrRand = new Random();
+                }
+
+                @Override
+                public void run(int i) throws Exception {
+                    double totWeight = 0;
+                    //Random rand = new Random();
+                    SolutionVbl localSolution = employedBees[i];
+                    totWeight += localSolution.item.exploitSolution(thrRand);
+                }
+            });
+        }
     }
 }
